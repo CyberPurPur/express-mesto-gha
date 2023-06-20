ef33178f7cbc606768b992a49fd7e0d311699422
Nadezhda Orlova <n-orlova@yandex.ru>
2023-06-20 22:49:33 +0300

    it was all about routes

--- routes/cards.js
+++ routes/cards.js
@@ -6,8 +6,8 @@ const {
 
 cardRouter.get('/', getCards);
 cardRouter.post('/', createCard);
-cardRouter.delete('/cards/:cardId', deleteCard);
-cardRouter.put('/cards/:cardId/likes', likeCard);
-cardRouter.delete('/cards/:cardId/likes', dislikeCard);
+cardRouter.delete('/:cardId', deleteCard);
+cardRouter.put('/:cardId/likes', likeCard);
+cardRouter.delete('/:cardId/likes', dislikeCard);
 
 module.exports = cardRouter;
--- routes/router.js
+++ routes/router.js
@@ -4,5 +4,8 @@ const cardRouter = require('./cards');
 
 router.use('/users', usersRouter);
 router.use('/cards', cardRouter);
+router.use('/*', (req, res) => {
+  res.status(404).send({ message: '404 not found' });
+});
 
 module.exports = router;
