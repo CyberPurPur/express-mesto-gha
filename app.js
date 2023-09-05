86bcebdf906fdbf29d4f2e72cd116756d8423700
Nadezhda Orlova <n-orlova@yandex.ru>
2023-09-05 23:32:32 +0300

    cookie

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-// eslint-disable-next-line import/no-unresolved
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
